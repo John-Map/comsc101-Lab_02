363ef7667813117835332c026e809d47e275e8ad
John-Map <JohnmarJimenez5@gmail.com>
2025-05-20 21:00:31 -0400

    Added subtraction and changed questions for input

--- Lab02.py
+++ Lab02.py
@@ -1,25 +1,28 @@
-#Part 1
 # I want this program to ask the user
-# their favorite month "What's your favorite month?" and then,
-# I want the program to say back "I like FAV_MONTH too" where FAV_MONTH is their favorite month
-#Part 2
-# I want this program to ask the user for a value
-# that says how old they are, "How old are you?" and then,
-# I want the program to say back "I am AGE years old as well" where
-# AGE is how old they are
+# the month they were born in "What month were you born in?" and then,
+# I want this program to ask the user how old they are, 
+# "How old are you?" and then,
+# I want the program to say back "You were born in MONTH and are AGE years old"
 
-#Part 1
-#step 1 ask for favorite month
-print("What is your favorite month?")
-#step 2 input the user's favorite month from the keyboard
-fav_month = input()
-#step 3 say i like back with their favorite month
-print("I like " + fav_month + " too")
+#step 1 ask for birth month
+print("What month were you on born?")
+#step 2 input the user's birth month from the keyboard
+Birth_month = input()
 
-#Part 2
-#step 1 as for their age
+#step 3 ask for their age
 print("How old are you?")
-#step 2 recieve their age as variable age
-age = input()
+#step 4 recieve their age as the variable age
+your_age = input()
 #step 3 say back i am AGE years old as well using their age
-print("I am " + str(age) + " years old as well")
+print("You were born on " + Birth_month + " and are " + your_age + " years old")
+
+# math we could do here is 
+# addition, subtraction, multiplication, 
+# division, modulo (divide & get remainder), 
+# Exponentiation (powers)
+# examples are: 
+# - calculating age or birth year
+# - converting between metric and imperial
+
+Century =  100 - int(your_age)
+print("You have " + str(Century) + " Year(s) Until you are 100 years old.")
